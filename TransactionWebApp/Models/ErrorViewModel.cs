3ae34a8400ae4f8222e91a0728f26fbe34e4b3e0
ymurshed <yaad.murshed@outlook.com>
2019-10-31 01:57:18 +0600

    Add project template.

---
+++ TransactionWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TransactionWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
